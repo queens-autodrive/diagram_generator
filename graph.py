d4f347bb8387b22420db66e9805c95373559cfad
Justin Frank <justin462@protonmail.com>
2024-07-28 15:15:27 -0400

    workspace framework layed out

--- graph.py
+++ graph.py
@@ -7,10 +7,12 @@ from collections import OrderedDict
 with open('README.md', 'r') as file:
     tree = markdown_to_json.dictify(file.read())
 
-nodes = OrderedDict()
-for key in tree['ROS2 Interactions'].keys():
-    if key.startswith("Node: "):
-        nodes[key.split('Node: ')[1]] = tree['ROS2 Interactions'][key]
+if 'Packages' in tree.keys():
+    repo_type = 'workspace'
+elif 'Workspaces' in tree.keys():
+    repo_type = 'package'
+else:
+    raise Exception
 
 
 def add_edge(graph, start, end, label, pub=True):
@@ -20,7 +22,7 @@ def add_edge(graph, start, end, label, pub=True):
         graph.edge(end, start, label=label)
 
 
-def check_interaction(graph, node, name, pub, prefix=''):
+def check_interaction(graph, key, node, name, pub, prefix=''):
     if name not in node.keys():
         print("Warning! Interaction name not found")
         return
@@ -31,23 +33,36 @@ def check_interaction(graph, node, name, pub, prefix=''):
         for subnode in subnodes:
             subnode = subnode.split(": ")[-1].split("'")[0]
             for subsubnode in subnode.split(', '):
-                add_edge(dot, key, subsubnode, f"{prefix}{pubkey.split('/')[-1]}", pub)
+                add_edge(graph, key, subsubnode, f"{prefix}{pubkey.split('/')[-1]}", pub)
+
+
+def addPackage(graph, tree):
+    nodes = OrderedDict()
+    for key in tree['ROS2 Interactions'].keys():
+        if key.startswith("Node: "):
+            nodes[key.split('Node: ')[1]] = tree['ROS2 Interactions'][key]
+
+    for key in nodes.keys():
+        graph.node(key, key, shape="ellipse", penwidth='3.0', fontsize="24pt")
+        node = nodes[key]
+        check_interaction(dot, key, node, 'Publishers', True)
+        check_interaction(dot, key, node, 'Subscriptions', False)
+        check_interaction(dot, key, node, 'Service Clients', True, 'Request ')
+        check_interaction(dot, key, node, 'Service Clients', False, 'Response ')
+        check_interaction(dot, key, node, 'Service Servers', True, 'Request ')
+        check_interaction(dot, key, node, 'Service Servers', False, 'Response ')
+        check_interaction(dot, key, node, 'Action Clients', True, 'Goal ')
+        check_interaction(dot, key, node, 'Action Clients', False, 'Result ')
+        check_interaction(dot, key, node, 'Action Servers', True, 'Goal ')
+        check_interaction(dot, key, node, 'Action Servers', False, 'Result ')
 
 
 dot = graphviz.Digraph(comment='System Architecture', strict=True)
-for key in nodes.keys():
-    dot.node(key, key, shape="ellipse", penwidth='3.0', fontsize="24pt")
-    node = nodes[key]
-    check_interaction(dot, node, 'Publishers', True)
-    check_interaction(dot, node, 'Subscriptions', False)
-    check_interaction(dot, node, 'Service Clients', True, 'Request ')
-    check_interaction(dot, node, 'Service Clients', False, 'Response ')
-    check_interaction(dot, node, 'Service Servers', True, 'Request ')
-    check_interaction(dot, node, 'Service Servers', False, 'Response ')
-    check_interaction(dot, node, 'Action Clients', True, 'Goal ')
-    check_interaction(dot, node, 'Action Clients', False, 'Result ')
-    check_interaction(dot, node, 'Action Servers', True, 'Goal ')
-    check_interaction(dot, node, 'Action Servers', False, 'Result ')
-
-
+if repo_type == 'package':
+    addPackage(dot, tree)
+else:
+    print(tree['Packages'])
+    for package in tree['Packages'].keys():
+        print(package)
+        print(tree['Packages'][package])
 dot.render(format='svg', filename='system_architecture', engine='dot')
